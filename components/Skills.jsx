e4b0c0ba1a62f7cfffb04f407e0a282c0d512d0f
Leo Curdi <ldcurdi@icloud.com>
2023-03-13 16:52:27 -0700

    .

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -17,7 +17,7 @@ const Skills = () => {
                     <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-200'>
                         <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                             <div className='m-auto'>
-                            <Image src='/../public/assets/skill-icons/html.png' width='64' height='64' alt='/' />
+                            <Image src='assets/skill-icons/html.png' width='64' height='64' alt='/' />
                             </div>
                             <div className='flex flex-col items-center justify-center'>
                                 <h3>HTML</h3>
