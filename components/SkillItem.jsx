2c8ddcdc870b2c34bf3f26fec17e98f93997f406
Leo Curdi <ldcurdi@icloud.com>
2023-04-02 13:06:49 -0700

    removed hover effect from skill items

--- components/SkillItem.jsx
+++ components/SkillItem.jsx
@@ -9,7 +9,7 @@ import {BsFillPersonLinesFill} from 'react-icons/bs'
 const SkillItem = ({backgroundImg, name}) => {
     return (
 
-        <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-200'>
+        <div className='p-6 shadow-xl rounded-xl' /* hover:scale-105 ease-in duration-200' */>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                 <div className='m-auto'>
                 <Image src={backgroundImg} width='64' height='64' alt='/' />
