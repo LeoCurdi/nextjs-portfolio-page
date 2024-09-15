ab91f848cd8474c48b8887c8b87959ee3e2a0c02
LeoCurdi <ldcurdi@icloud.com>
2024-09-15 01:16:55 -0700

    updated bio

--- components/About.jsx
+++ components/About.jsx
@@ -10,8 +10,10 @@ const About = () => {
                 <div className='col-span-2'>
                     <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
                     <h2 className='py-4'>Who I Am</h2>
-                    <p className='py-2 text-gray-600'> { "I'm currently a college student pursuing a bachelor's degree in Computer Science at Washington State University, but I'm looking for a developer / software engineer position while I finish my degree. I've had an interest in coding ever since 2013 when I started playing around with Minecraft mods and messing with the function of the game. My interests include artificial intelligence, data science, and machine learning. I'm also passionate about investing, business, and finance." } </p>
-                    <p className='py-2 text-gray-600'> { "Lately, I've been focusing on web development. I'm teaching myself how to build responsive sites with pleasing UI's that utilize backend technologies such as API's. Currently, I am most proficient in building front-end applications using HTML, CSS, Javascript, and React, but I am a quick learner and can pick up new tech stacks as needed." } </p>
+                    <p className='py-2 text-gray-600'> { "I'm a senior at Washington State University, pursuing a BS in Computer Science. My passion for coding began about ten years ago when I started experimenting with Minecraft mods and tweaking the function of the game." } </p>
+                    <p className='py-2 text-gray-600'> { "Recently, I've been focusing on cybersecurity, computer networks, and machine learning, though I have a strong foundation in software engineering and full-stack web development. In my personal projects, I've built robust, well-tested software, and developed responsive sites that utilize backend technologies. I'm most proficient in C# and WinForms, having developed a Microsoft Excel clone. Additionally, I've built projects leveraging Node.js, MongoDB, Express, and React, and I'm always ready to adapt to new tech stacks as needed." } </p>
+                    <p className='py-2 text-gray-600'> { "My additional interests extend to cloud computing, data science, business, and investing." } </p>
+                    <p className='py-2 text-gray-600'> { "In my free time, I enjoy climbing, snowboarding, and capturing my adventures through filmmaking." } </p>
                     <Link href='/#projects'>
                         <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects</p>
                     </Link>
